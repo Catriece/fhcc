441baea297b4d3f4cd0fe3bf1e211fcd68b86c84
Catriece Gilbert <119451056+Catriece@users.noreply.github.com>
2024-10-04 17:11:56 -0400

    added type declarations

---
+++ src/types/declarations.d.ts
@@ -0,0 +1,21 @@
+// src/types/declarations.d.ts
+
+declare module "./pages/home/home.tsx" {
+  const content: any; // You can specify a more specific type here if known
+  export default content;
+}
+
+declare module "./pages/about/about.tsx" {
+  const content: any; // You can specify a more specific type here if known
+  export default content;
+}
+
+declare module "./pages/contact/contact.tsx" {
+  const content: any; // You can specify a more specific type here if known
+  export default content;
+}
+
+declare module "./pages/services/services.tsx" {
+  const content: any; // You can specify a more specific type here if known
+  export default content;
+}
